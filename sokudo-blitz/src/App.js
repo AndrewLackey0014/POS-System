d2f0dc0c313c5491e9a4cbcf036adb72426a0673
Sai Juttu <saijuttu@tamu.edu>
2022-11-07 17:50:05 -0600

    working nav bar

--- sokudo-blitz/src/App.js
+++ sokudo-blitz/src/App.js
@@ -1,11 +1,28 @@
+import NavBar from "./NavBar";
+import Pricing from "./pages/Pricing"
+import About from "./pages/About"
+import Home from "./pages/Home"
 
 function App() {
+  let component
+  switch (window.location.pathname) {
+    case "/pricing":
+      component = <Pricing/>
+      break
+    case "/about":
+      component = <About/>
+      break
+    default: 
+      component = <Home/>
+      break
+  }
   return (
+    <>
+      <NavBar/>
+      {component}
+    </>
+  )
 
-    <div>
-      Sokudo
-    </div>
-  );
 }
 
 export default App;
