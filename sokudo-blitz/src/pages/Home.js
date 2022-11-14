9398b12f071028fb731c8a864cec6dd85472bed8
AndrewLackey <andrewlackey@tamu.edu>
2022-11-14 08:22:35 -0600

    added logo to home page

--- sokudo-blitz/src/pages/Home.js
+++ sokudo-blitz/src/pages/Home.js
@@ -4,7 +4,7 @@ import "./home.css"
 export default function Home() {
     return (
         <body>
-            
+            <img alt = "Cabo Grill logo" title = "logo" className = "logo" src="https://api.dineoncampus.com/files/images/fb0f93d4-8920-4567-b1e6-70b3ee72690c.png" />
             Welcome to Cabo Grill!
             <br></br>
             
