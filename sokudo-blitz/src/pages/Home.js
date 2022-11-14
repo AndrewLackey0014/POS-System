108bb8688892bf90119bda952e884077f2b24a7a
AndrewLackey <andrewlackey@tamu.edu>
2022-11-14 08:33:01 -0600

    added logo and changed formatting of home page

--- sokudo-blitz/src/pages/Home.js
+++ sokudo-blitz/src/pages/Home.js
@@ -5,19 +5,16 @@ export default function Home() {
     return (
         <body>
             <img alt = "Cabo Grill logo" title = "logo" className = "logo" src="https://api.dineoncampus.com/files/images/fb0f93d4-8920-4567-b1e6-70b3ee72690c.png" />
-            Welcome to Cabo Grill!
-            <br></br>
-            
+            <p className = "center">Welcome to Cabo Grill!</p>
             <br></br>
 
+            <li className = "center"><a href="CustomerGUI">Customer GUI</a></li>
             <br></br>
-            <li><a href="CustomerGUI">Customer GUI</a></li>
+            <li className = "center"><a href="ManagerGUI">Manager GUI</a></li>
             <br></br>
-            <li><a href="ManagerGUI">Manager GUI</a></li>
+            <li className = "center"><a href="ServerGUI">Server GUI</a></li>
             <br></br>
-            <li><a href="ServerGUI">Server GUI</a></li>
-            <br></br>
-            <li><a href="About">About us</a></li>
+            <li className = "center"><a href="About">About us</a></li>
         </body>
     );
 }
