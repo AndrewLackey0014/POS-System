597cca477424fd30e69fc68c2e37dca9270d8a63
tarun7467 <tarun7467@tamu.edu>
2022-11-18 05:11:55 -0600

    Updating live data

--- sokudo-blitz/src/pages/Home.js
+++ sokudo-blitz/src/pages/Home.js
@@ -5,16 +5,16 @@ export default function Home() {
     return (
         <body>
             <img alt = "Cabo Grill logo" title = "logo" className = "logo" src="https://api.dineoncampus.com/files/images/fb0f93d4-8920-4567-b1e6-70b3ee72690c.png" />
-            <p className = "center">Welcome to Cabo Grill!</p>
+            <h1 className = "center">WELCOME TO CABO GRILL</h1>
             <br></br>
 
-            <li className = "list"><a href="CustomerGUI">Customer GUI</a></li>
+            <button className = "list"><a href="CustomerGUI">CUSTOMER GUI</a></button>
             <br></br>
-            <li className = "list"><a href="ManagerGUI">Manager GUI</a></li>
+            <button className = "list"><a href="ManagerGUI">MANAGER GUI</a></button>
             <br></br>
-            <li className = "list"><a href="ServerGUI">Server GUI</a></li>
+            <button className = "list"><a href="ServerGUI">SERVER GUI</a></button>
             <br></br>
-            <li className = "list"><a href="About">About us</a></li>
+            <button className = "list"><a href="About">ABOUT US</a></button>
         </body>
     );
 }
