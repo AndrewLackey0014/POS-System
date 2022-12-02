935d0c5c78a83ae9d53b2fbbc148cd64b001aabd
Andrew Lackey <Andrewlackey@tamu.edu>
2022-12-02 16:05:16 -0600

    fixed formatting of order content and added modifiers

--- sokudo-blitz/src/pages/CustomerGUI.js
+++ sokudo-blitz/src/pages/CustomerGUI.js
@@ -39,38 +39,63 @@ var order_summary = [];
 
 
     const handleTaco = (e)=>{
-        order_summary.push("Taco");
-
+        if (order_summary.length == 0){
+          order_summary.push("Taco");
+        }else{
+          order_summary.push(", Taco");
+        }
     }
     const handleBurrito = (e)=>{
     //   alert("Taco")
-      order_summary.push("Burrito");
+      if (order_summary.length == 0){
+        order_summary.push("Burrito")
+      }else{
+        order_summary.push(", Burrito");
+      } 
 
     }
     const handleSalad = (e)=>{
-      <a href="Toppings_Page" className="naventries">Toppings</a>
-    //   alert("Taco")
-      order_summary.push("Salad");
+      if (order_summary.length == 0){
+        order_summary.push("Salad");
+      }else{
+        order_summary.push(", Salad");
+      }
 
     }
     const handleBowl = (e)=>{
-      <a href="/CustomerGUI/Toppings_Page" className="naventries">Toppings</a>
-      order_summary.push("Bowl");
+      if (order_summary.length == 0){
+        order_summary.push("Bowl");
+      }else{
+        order_summary.push(", Bowl");
+      }
     }
     const handleDrink= (e)=>{
+      if (order_summary.length == 0){
         order_summary.push("Drink");
+      }else{
+        order_summary.push(", Drink");
+      }
     }
     const handleChips_and_Salsa= (e)=>{
-        //alert("Taco")
+      if (order_summary.length == 0){
         order_summary.push("Chips&Salsa");
+      }else{
+        order_summary.push(", Chips&Salsa");
+      }
     }
     const handleChips_and_Queso= (e)=>{
-       // alert("Taco")
+      if (order_summary.length == 0){
         order_summary.push("Chips&Queso");
+      }else{
+        order_summary.push(", Chips&Queso");
+      }
     }
     const handleChips_and_Guac= (e)=>{
-       // alert("Taco")
+      if (order_summary.length == 0){
         order_summary.push("Chips&Guac");
+      }else{
+        order_summary.push(", Chips&Guac");
+      }
     }
 
 
@@ -84,7 +109,7 @@ var order_summary = [];
 
     const handleOrder = (e)=>{
 
-        stringorder= order_summary.toString();
+        stringorder= order_summary.join(" ");
         let Contents = stringorder;
         document.getElementById("Contents").innerHTML = Contents;
 
@@ -98,6 +123,42 @@ var order_summary = [];
 
 
     }
+
+    const handleBeef = (e)=>{
+      order_summary.push("Beef");
+    }
+    const handleSteak = (e)=>{
+      order_summary.push("Steak");
+    }
+    const handleMedley = (e)=>{
+      order_summary.push("Medley");
+    }
+    const handleChicken = (e)=>{
+      order_summary.push("Chicken");
+    }
+    const handleCheese = (e)=>{
+      order_summary.push("Cheese");
+    }
+    const handleBeans = (e)=>{
+      order_summary.push("Beans");
+    }
+    const handleRice = (e)=>{
+      order_summary.push("Rice");
+    }
+    const handleSalsa = (e)=>{
+      order_summary.push("Salsa");
+    }
+    const handleCream = (e)=>{
+      order_summary.push("SourCream");
+    }
+    const handleGuac = (e)=>{
+      order_summary.push("Guac");
+    }
+    const handleQueso = (e)=>{
+      order_summary.push("Queso");
+    }
+
+
       return (
         /*need to add on click event to show a menu customization options such as meat option and topping
         Should make it to where the other items disappear so you can customize then add then unhide and hide the customizations*/
@@ -112,38 +173,23 @@ var order_summary = [];
                   <p>Tacos</p>
               </button>
                 <div class="dropdown-content">
-                <input type="checkbox" /> Beef
-                <input type="checkbox" />Steak
-                <input type="checkbox" />Veggie Medley
-                <input type="checkbox" />Chicken
-                <input type="checkbox" />Cheese
-                <input type="checkbox" />Beans
-                <input type="checkbox" />Rice
-                <input type="checkbox" />Salsa
-                <input type="checkbox" />Sour Cream
-                <input type="checkbox" />Guacamole
-                <input type="checkbox" />Queso
+                  <button onClick={handleBeef}><p>Beef</p></button>
+                  <button onClick={handleSteak}><p>Steak</p></button>
+                  <button onClick={handleMedley}><p>Veggie Medley</p></button>
+                  <button onClick={handleChicken}><p>Chicken</p></button>
+                  <button onClick={handleCheese}><p>Cheese</p></button>
+                  <button onClick={handleBeans}><p>Beans</p></button>
+                  <button onClick={handleRice}><p>Rice</p></button>
+                  <button onClick={handleSalsa}><p>Salsa</p></button>
+                  <button onClick={handleCream}><p>Sour Cream</p></button>
+                  <button onClick={handleGuac}><p>Guacamole</p></button>
+                  <button onClick={handleQueso}><p>Queso</p></button>
                 </div>
               </div>
-              <div class="dropdown">
-              <button onClick = {handleBowl}>
-                  <img alt = "bowl" title = "bowl" className = "mainItems" src="https://www.threelittlechickpeas.com/wp-content/uploads/2020/01/vegan-taco-bowl-with-lime-crema.jpg" />
-                  <p>Bowl</p>
+              <button onClick = {handleDrink}>
+                  <img alt = "drink" title = "drink" className = "mainItems" src="https://media.istockphoto.com/id/467108213/photo/red-cardboard-cup-with-a-straw.jpg?s=612x612&w=0&k=20&c=tkx8YkdZTYJWaslh571sKof3v78yrl_HapEvTawfGDE=" />
+                  <p>Drink</p>
               </button>
-                <div class="dropdown-content">
-                <input type="checkbox" />Beef
-                <input type="checkbox" />Steak
-                <input type="checkbox" />Veggie Medley
-                <input type="checkbox" />Chicken
-                <input type="checkbox" />Cheese
-                <input type="checkbox" />Beans
-                <input type="checkbox" />Rice
-                <input type="checkbox" />Salsa
-                <input type="checkbox" />Sour Cream
-                <input type="checkbox" />Guacamole
-                <input type="checkbox" />Queso
-                </div>
-              </div>
             </div>
             <div className = "columnCust">
             <div class="dropdown">
@@ -152,54 +198,69 @@ var order_summary = [];
                   <p>Burrito</p>
               </button>
                 <div class="dropdown-content">
-                <input type="checkbox" />Beef
-                <input type="checkbox" />Steak
-                <input type="checkbox" />Veggie Medley
-                <input type="checkbox" />Chicken
-                <input type="checkbox" />Cheese
-                <input type="checkbox" />Beans
-                <input type="checkbox" />Rice
-                <input type="checkbox" />Salsa
-                <input type="checkbox" />Sour Cream
-                <input type="checkbox" />Guacamole
-                <input type="checkbox" />Queso
+                  <button onClick={handleBeef}><p>Beef</p></button>
+                  <button onClick={handleSteak}><p>Steak</p></button>
+                  <button onClick={handleMedley}><p>Veggie Medley</p></button>
+                  <button onClick={handleChicken}><p>Chicken</p></button>
+                  <button onClick={handleCheese}><p>Cheese</p></button>
+                  <button onClick={handleBeans}><p>Beans</p></button>
+                  <button onClick={handleRice}><p>Rice</p></button>
+                  <button onClick={handleSalsa}><p>Salsa</p></button>
+                  <button onClick={handleCream}><p>Sour Cream</p></button>
+                  <button onClick={handleGuac}><p>Guacamole</p></button>
+                  <button onClick={handleQueso}><p>Queso</p></button>
                 </div>
                 </div>
-                <div class="dropdown">
-                <button onClick = {handleSalad}>
-                  <img alt = "salad" title = "salad" className = "mainItems" src="https://assets.bonappetit.com/photos/624215f8a76f02a99b29518f/3:4/w_1812,h_2416,c_limit/0328-ceasar-salad-lede.jpg" />
-                  <p>Salad</p>
+              <button onClick = {handleChips_and_Queso}>
+                  <img alt = "chips and queso" title = "chips and queso" className = "mainItems" src="https://www.tacobueno.com/assets/food/nachos/Nachos_chips_queso_990x725.jpg" />
+                  <p>Chips and Queso</p>
               </button>
-                <div class="dropdown-content">
-                <input type="checkbox" />Beef
-                <input type="checkbox" />Steak
-                <input type="checkbox" />Veggie Medley
-                <input type="checkbox" />Chicken
-                <input type="checkbox" />Cheese
-                <input type="checkbox" />Beans
-                <input type="checkbox" />Rice
-                <input type="checkbox" />Salsa
-                <input type="checkbox" />Sour Cream
-                <input type="checkbox" />Guacamole
-                <input type="checkbox" />Queso
-                </div>
-                </div>
             </div>
             <div className = "columnCust">
-              <button onClick = {handleDrink}>
-                  <img alt = "drink" title = "drink" className = "mainItems" src="https://media.istockphoto.com/id/467108213/photo/red-cardboard-cup-with-a-straw.jpg?s=612x612&w=0&k=20&c=tkx8YkdZTYJWaslh571sKof3v78yrl_HapEvTawfGDE=" />
-                  <p>Drink</p>
+              <div class="dropdown">
+              <button onClick = {handleBowl}>
+                  <img alt = "bowl" title = "bowl" className = "mainItems" src="https://www.threelittlechickpeas.com/wp-content/uploads/2020/01/vegan-taco-bowl-with-lime-crema.jpg" />
+                  <p>Bowl</p>
               </button>
+                <div class="dropdown-content">
+                  <button onClick={handleBeef}><p>Beef</p></button>
+                  <button onClick={handleSteak}><p>Steak</p></button>
+                  <button onClick={handleMedley}><p>Veggie Medley</p></button>
+                  <button onClick={handleChicken}><p>Chicken</p></button>
+                  <button onClick={handleCheese}><p>Cheese</p></button>
+                  <button onClick={handleBeans}><p>Beans</p></button>
+                  <button onClick={handleRice}><p>Rice</p></button>
+                  <button onClick={handleSalsa}><p>Salsa</p></button>
+                  <button onClick={handleCream}><p>Sour Cream</p></button>
+                  <button onClick={handleGuac}><p>Guacamole</p></button>
+                  <button onClick={handleQueso}><p>Queso</p></button>
+                </div>
+              </div>
               <button onClick = {handleChips_and_Salsa}>
                   <img alt = "chips and salsa" title = "chips and salsa" className = "mainItems" src="https://media.chefdehome.com/740/0/0/salsa/homemade-salsa.jpg" />
                   <p>Chips and Salsa</p>
               </button>
             </div>
             <div className = "columnCust">
-              <button onClick = {handleChips_and_Queso}>
-                  <img alt = "chips and queso" title = "chips and queso" className = "mainItems" src="https://www.tacobueno.com/assets/food/nachos/Nachos_chips_queso_990x725.jpg" />
-                  <p>Chips and Queso</p>
+              <div class="dropdown">
+                <button onClick = {handleSalad}>
+                  <img alt = "salad" title = "salad" className = "mainItems" src="https://assets.bonappetit.com/photos/624215f8a76f02a99b29518f/3:4/w_1812,h_2416,c_limit/0328-ceasar-salad-lede.jpg" />
+                  <p>Salad</p>
               </button>
+                <div class="dropdown-content">
+                  <button onClick={handleBeef}><p>Beef</p></button>
+                  <button onClick={handleSteak}><p>Steak</p></button>
+                  <button onClick={handleMedley}><p>Veggie Medley</p></button>
+                  <button onClick={handleChicken}><p>Chicken</p></button>
+                  <button onClick={handleCheese}><p>Cheese</p></button>
+                  <button onClick={handleBeans}><p>Beans</p></button>
+                  <button onClick={handleRice}><p>Rice</p></button>
+                  <button onClick={handleSalsa}><p>Salsa</p></button>
+                  <button onClick={handleCream}><p>Sour Cream</p></button>
+                  <button onClick={handleGuac}><p>Guacamole</p></button>
+                  <button onClick={handleQueso}><p>Queso</p></button>
+                </div>
+                </div>
               <button onClick = {handleChips_and_Guac}>
                   <img alt = "chips and quac" title = "chips and quacamole" className = "mainItems" src="https://brokebankvegan.com/wp-content/uploads/2021/06/Guacamole-Feature.jpg" />
                   <p>Chips and Guacamole</p>
